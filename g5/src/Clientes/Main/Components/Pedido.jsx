b8e9e297c3bb7c3968d39bc27ea99fc9d841dcc9
GarroSebastian <garrosebastian07@gmail.com>
2023-02-02 17:25:46 -0500

    Añadir login rest

--- g5/src/Clientes/Main/Components/Pedido.jsx
+++ g5/src/Clientes/Main/Components/Pedido.jsx
@@ -1,50 +1,50 @@
 
 function Pedido(){
-    return <body class="bg-light">
-    <div class="container">
-        <div class="py-5 text-center">
-            <img class="mb-4 d-block mx-auto"
+    return <body className="bg-light">
+    <div className="container">
+        <div className="py-5 text-center">
+            <img className="mb-4 d-block mx-auto"
                 src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Universidad_de_Lima_logo.png" alt="Bootstrap"
                 width="50" height="50"/>
             
             <h2>Pedido</h2>
-            <p class="lead">Complete la información para realizar su pedido</p>
+            <p className="lead">Complete la información para realizar su pedido</p>
         </div>
     </div>
-    <div class="container">
-        <h4 class="mb-3">Nombre</h4>
+    <div className="container">
+        <h4 className="mb-3">Nombre</h4>
         <form>
-            <div class="row g-3">
-                <div class="col-sm-6">
-                    <label for="firstName" class="form-label">Primer Nombre</label>
-                    <input id="firstName" type="text" class="form-control" required/>
-                    <div class="invalid-feedback">
+            <div className="row g-3">
+                <div className="col-sm-6">
+                    <label for="firstName" className="form-label">Primer Nombre</label>
+                    <input id="firstName" type="text" className="form-control" required/>
+                    <div className="invalid-feedback">
                         Primer nombre es necesario.
                     </div>
                 </div>
-                <div class="col-sm-6">
-                    <label for="lastName" class="form-label">Apellido</label>
-                    <input id="lastName" type="text" class="form-control" required/>
-                    <div class="invalid-feedback">
+                <div className="col-sm-6">
+                    <label for="lastName" className="form-label">Apellido</label>
+                    <input id="lastName" type="text" className="form-control" required/>
+                    <div className="invalid-feedback">
                         Apellido es necesario.
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div className="col-md-4">
                     <label for="edificio"
-                    class="form-label">Edificio</label>
+                    className="form-label">Edificio</label>
                     <select id="edificio"
-                    class="form-control">
+                    className="form-control">
                     <option value="">Elegir...
                     </option>
                     <option value="L3">L3</option>
                 </select>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div className="col-md-4">
             <label for="Aula"
-                    class="form-label">Aula/Sala</label>
+                    className="form-label">Aula/Sala</label>
                     <select id="Aula"
-                    class="form-control">
+                    className="form-control">
                     <option value="">Elegir...
                     </option>
                     <option value="302">302</option>
--- g5/src/Restaurantes/Login/LoginPage.jsx
+++ g5/src/Restaurantes/Login/LoginPage.jsx
@@ -1,7 +1,42 @@
 import { useNavigate} from 'react-router-dom'
-
+import LoginForm from './Components/LoginForm'
 function LoginPage(){
+    const navigate = useNavigate()
 
+    const onLoginOk = function(
+        usuario, password
+    ) {
+        if (usuario === "restaurante"
+            && password === "123"){
+
+            const dataUsuario = {
+                username : usuario,
+                password: password
+            }
+
+            const dataUsuarioJSON = JSON.stringify(dataUsuario)
+            console.log(dataUsuario)
+            console.log(dataUsuarioJSON)
+
+            sessionStorage.setItem("DATA_USUARIO", dataUsuarioJSON)
+
+
+            navigate("/principal", {
+                state: {
+                    username: usuario
+                }
+            })
+        }
+    }
+    
+        return <div className="row">
+                <div className="col"></div>
+                <div className="col">
+                    <LoginForm
+                    onLoginOk= { onLoginOk}/>
+                </div>
+                <div className="col"></div>
+        </div>             
 }
 
 export default LoginPage
